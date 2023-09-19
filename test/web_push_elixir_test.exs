193a12e1efe6abfccd551d55ceba3148f86b7f7b
trueChazza <chaz.goodare@gmail.com>
2023-09-19 18:29:29 +1200

    test: add decode

--- test/web_push_elixir_test.exs
+++ test/web_push_elixir_test.exs
@@ -1,8 +1,16 @@
 defmodule WebPushElixirTest do
   use ExUnit.Case
-  doctest WebPushElixir
 
-  test "greets the world" do
-    assert WebPushElixir.hello() == :world
+  test "it should decode" do
+    assert Jason.decode!(
+             '{"endpoint":"https://some.pushservice.com/something-unique","keys":{"p256dh":"BIPUL12DLfytvTajnryr2PRdAgXS3HGKiLqndGcJGabyhHheJYlNGCeXl1dn18gSJ1WAkAPIxr4gK0_dQds4yiI=","auth":"FPssNDTKnInHVndSTdbKFw=="}}'
+           ) == %{
+             "endpoint" => "https://some.pushservice.com/something-unique",
+             "keys" => %{
+               "auth" => "FPssNDTKnInHVndSTdbKFw==",
+               "p256dh" =>
+                 "BIPUL12DLfytvTajnryr2PRdAgXS3HGKiLqndGcJGabyhHheJYlNGCeXl1dn18gSJ1WAkAPIxr4gK0_dQds4yiI="
+             }
+           }
   end
 end
