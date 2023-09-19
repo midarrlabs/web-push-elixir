d89c96cc63f3d7d53d028c050dc0b01cdf957acb
trueChazza <chaz.goodare@gmail.com>
2023-09-19 18:06:06 +1200

    initial commit

---
+++ lib/web_push_elixir.ex
@@ -0,0 +1,18 @@
+defmodule WebPushElixir do
+  @moduledoc """
+  Documentation for `WebPushElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WebPushElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/web_push_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule WebPushElixirTest do
+  use ExUnit.Case
+  doctest WebPushElixir
+
+  test "greets the world" do
+    assert WebPushElixir.hello() == :world
+  end
+end
