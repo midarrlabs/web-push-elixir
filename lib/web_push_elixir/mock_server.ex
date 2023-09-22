1f7625a73d3853d7512a56e9020a33acdc392830
trueChazza <chaz.goodare@gmail.com>
2023-09-22 17:21:26 +1200

    feat: add example

--- lib/web_push_elixir/mock_server.ex
+++ lib/web_push_elixir/mock_server.ex
@@ -8,4 +8,34 @@ defmodule WebPushElixir.MockServer do
     conn
     |> Plug.Conn.send_resp(200, "ok")
   end
+
+  get "/example" do
+    conn
+    |> put_resp_header("content-type", "text/html; charset=utf-8")
+    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/index.html")
+  end
+
+  get "/app.webmanifest" do
+    conn
+    |> put_resp_header("content-type", "application/manifest+json")
+    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/app.webmanifest")
+  end
+
+  get "/main.js" do
+    conn
+    |> put_resp_header("content-type", "application/x-javascript")
+    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/main.js")
+  end
+
+  get "/service-worker.js" do
+    conn
+    |> put_resp_header("content-type", "application/x-javascript")
+    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/service-worker.js")
+  end
+
+  get "/favicon.ico" do
+    conn
+    |> put_resp_header("content-type", "image/x-icon")
+    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/favicon.ico")
+  end
 end
