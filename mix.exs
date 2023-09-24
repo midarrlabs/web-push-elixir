82c2532cb62171dc8b73a483678c04d29c56e5e9
trueChazza <chaz.goodare@gmail.com>
2023-09-24 15:37:01 +1300

    build: add ex doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule WebPushElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Simple web push for Elixir",
+      package: [
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/midarrlabs/web-push-elixir"}
+      ],
+
     ]
   end
 
@@ -22,8 +28,7 @@ defmodule WebPushElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:jose, "~> 1.11"},
       {:jason, "~> 1.4"},
       {:plug, "~> 1.14"},
