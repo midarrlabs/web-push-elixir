57755799c6bcac7fd5cf65d1a60274e39fc6cdb8
trueChazza <chaz.goodare@gmail.com>
2023-09-25 14:52:18 +1300

    refactor: improve readability

--- lib/web_push_elixir.ex
+++ lib/web_push_elixir.ex
@@ -9,6 +9,57 @@ defmodule WebPushElixir do
     Base.url_decode64!(string, padding: false)
   end
 
+  defp hkdf(salt, ikm, info, length) do
+    prk =
+      :crypto.mac_init(:hmac, :sha256, salt)
+      |> :crypto.mac_update(ikm)
+      |> :crypto.mac_final()
+
+    :crypto.mac_init(:hmac, :sha256, prk)
+    |> :crypto.mac_update(info)
+    |> :crypto.mac_update(<<1>>)
+    |> :crypto.mac_final()
+    |> :binary.part(0, length)
+  end
+
+  defp encrypt(
+         message,
+         %{keys: %{auth: auth, p256dh: p256dh}} = _subscription,
+         server_public_key,
+         server_private_key
+       ) do
+    client_public_key = url_decode(p256dh)
+    client_auth_secret = url_decode(auth)
+
+    salt = :crypto.strong_rand_bytes(16)
+
+    shared_secret = :crypto.compute_key(:ecdh, client_public_key, server_private_key, :prime256v1)
+
+    prk = hkdf(client_auth_secret, shared_secret, "Content-Encoding: auth" <> <<0>>, 32)
+
+    context =
+      <<0, byte_size(client_public_key)::unsigned-big-integer-size(16)>> <>
+        client_public_key <>
+        <<byte_size(server_public_key)::unsigned-big-integer-size(16)>> <> server_public_key
+
+    content_encryption_key_info = "Content-Encoding: aesgcm" <> <<0>> <> "P-256" <> context
+    content_encryption_key = hkdf(salt, prk, content_encryption_key_info, 16)
+
+    nonce = hkdf(salt, prk, "Content-Encoding: nonce" <> <<0>> <> "P-256" <> context, 12)
+
+    {cipher_text, cipher_tag} =
+      :crypto.crypto_one_time_aead(
+        :aes_128_gcm,
+        content_encryption_key,
+        nonce,
+        message,
+        "",
+        true
+      )
+
+    %{ciphertext: cipher_text <> cipher_tag, salt: salt}
+  end
+
   def gen_key_pair() do
     {public, private} = :crypto.generate_key(:ecdh, :prime256v1)
 
@@ -24,52 +75,6 @@ defmodule WebPushElixir do
     end
   end
 
-  defp hkdf(salt, ikm, info, length) do
-    prk =
-      :crypto.mac_init(:hmac, :sha256, salt)
-      |> :crypto.mac_update(ikm)
-      |> :crypto.mac_final()
-
-    :crypto.mac_init(:hmac, :sha256, prk)
-    |> :crypto.mac_update(info)
-    |> :crypto.mac_update(<<1>>)
-    |> :crypto.mac_final()
-    |> :binary.part(0, length)
-  end
-
-  defp encrypt(message, %{keys: %{auth: auth, p256dh: p256dh}} = _subscription, server_public_key, server_private_key) do
-    client_public_key = url_decode(p256dh)
-    client_auth_secret = url_decode(auth)
-
-    salt = :crypto.strong_rand_bytes(16)
-
-    shared_secret = :crypto.compute_key(:ecdh, client_public_key, server_private_key, :prime256v1)
-
-    prk = hkdf(client_auth_secret, shared_secret, "Content-Encoding: auth" <> <<0>>, 32)
-
-    context =
-      <<0, byte_size(client_public_key)::unsigned-big-integer-size(16)>> <>
-        client_public_key <>
-        <<byte_size(server_public_key)::unsigned-big-integer-size(16)>> <> server_public_key
-
-    content_encryption_key_info = "Content-Encoding: " <> "aesgcm" <> <<0>> <> "P-256" <> context
-    content_encryption_key = hkdf(salt, prk, content_encryption_key_info, 16)
-
-    nonce = hkdf(salt, prk, "Content-Encoding: " <> "nonce" <> <<0>> <> "P-256" <> context, 12)
-
-    {cipher_text, cipher_tag} =
-      :crypto.crypto_one_time_aead(
-        :aes_128_gcm,
-        content_encryption_key,
-        nonce,
-        message,
-        "",
-        true
-      )
-
-    %{ciphertext: cipher_text <> cipher_tag, salt: salt}
-  end
-
   def send_web_push(message, %{endpoint: endpoint} = subscription) do
     expiration_timestamp = DateTime.to_unix(DateTime.utc_now()) + 12 * 3600
 
@@ -77,16 +82,17 @@ defmodule WebPushElixir do
     private_key = url_decode(System.get_env("PRIVATE_KEY"))
 
     jwt_payload =
-      %{
+      JOSE.JWT.from_map(%{
         aud: URI.parse(endpoint).scheme <> "://" <> URI.parse(endpoint).host,
         exp: expiration_timestamp,
         sub: System.get_env("SUBJECT")
-      }
-      |> JOSE.JWT.from_map()
+      })
 
     jwk =
-      {:ECPrivateKey, 1, private_key, {:namedCurve, {1, 2, 840, 10045, 3, 1, 7}}, public_key, nil}
-      |> JOSE.JWK.from_key()
+      JOSE.JWK.from_key(
+        {:ECPrivateKey, 1, private_key, {:namedCurve, {1, 2, 840, 10045, 3, 1, 7}}, public_key,
+         nil}
+      )
 
     {%{alg: :jose_jws_alg_ecdsa}, jwt} =
       JOSE.JWS.compact(JOSE.JWT.sign(jwk, %{"alg" => "ES256"}, jwt_payload))
@@ -94,7 +100,7 @@ defmodule WebPushElixir do
     encrypted = encrypt(message, subscription, public_key, private_key)
 
     HTTPoison.post(endpoint, encrypted.ciphertext, %{
-      "Authorization" => "WebPush " <> jwt,
+      "Authorization" => "WebPush #{jwt}",
       "Content-Encoding" => "aesgcm",
       "Crypto-Key" => "dh=#{url_encode(public_key)};",
       "Encryption" => "salt=#{url_encode(encrypted.salt)}",
