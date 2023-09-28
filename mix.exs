82a951c89811ed87fd08f10aeac4cbadc18153df
trueChazza <chaz.goodare@gmail.com>
2023-09-29 10:35:51 +1300

    build: bump v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WebPushElixir.MixProject do
   def project do
     [
       app: :web_push_elixir,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
