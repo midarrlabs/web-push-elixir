05f565ba5b342d93b5855e7d7d58764722d8f739
trueChazza <chaz.goodare@gmail.com>
2023-09-29 10:19:29 +1300

    refactor: move example

--- lib/web_push_elixir/mock_server.ex
+++ lib/web_push_elixir/mock_server.ex
@@ -12,36 +12,36 @@ defmodule WebPushElixir.MockServer do
   get "/" do
     conn
     |> put_resp_header("content-type", "text/html; charset=utf-8")
-    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/index.html")
+    |> Plug.Conn.send_file(200, "./example/index.html")
   end
 
   get "/app.webmanifest" do
     conn
     |> put_resp_header("content-type", "application/manifest+json")
-    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/app.webmanifest")
+    |> Plug.Conn.send_file(200, "./example/app.webmanifest")
   end
 
   get "/main.js" do
     conn
     |> put_resp_header("content-type", "application/x-javascript")
-    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/main.js")
+    |> Plug.Conn.send_file(200, "./example/main.js")
   end
 
   get "/service-worker.js" do
     conn
     |> put_resp_header("content-type", "application/x-javascript")
-    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/service-worker.js")
+    |> Plug.Conn.send_file(200, "./example/service-worker.js")
   end
 
   get "/web-push-elixir/service-worker.js" do
     conn
     |> put_resp_header("content-type", "application/x-javascript")
-    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/service-worker.js")
+    |> Plug.Conn.send_file(200, "./example/service-worker.js")
   end
 
   get "/favicon.ico" do
     conn
     |> put_resp_header("content-type", "image/x-icon")
-    |> Plug.Conn.send_file(200, "./lib/web_push_elixir/favicon.ico")
+    |> Plug.Conn.send_file(200, "./example/favicon.ico")
   end
 end
