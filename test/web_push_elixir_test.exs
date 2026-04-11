f51e135db7bcb3d225891a751384e03dd208109e
Fabian Thoma @ WEST Solution <fabian.thoma@west-solution.ch>
2026-04-11 21:04:16 +0200

    Add tests for message options

--- test/web_push_elixir_test.exs
+++ test/web_push_elixir_test.exs
@@ -1,10 +1,16 @@
 defmodule WebPushElixirTest do
   use ExUnit.Case
 
+  @test_subscription %{
+    "endpoint" => "http://localhost:4040/some-push-service",
+    "keys" => %{
+      "p256dh" => "BNcRdreALRFXTkOOUHK1EtK2wtaz5Ry4YfYCA_0QTpQtUbVlUls0VJXg7A8u-Ts1XbjhazAkj7I99e8QcYP7DkM=",
+      "auth" => "tBHItJI5svbpez7KI4CCXg=="
+      }
+    }
+
   test "it should send notification" do
-      subscription = """
-      {"endpoint":"http://localhost:4040/some-push-service","keys":{"p256dh":"BNcRdreALRFXTkOOUHK1EtK2wtaz5Ry4YfYCA_0QTpQtUbVlUls0VJXg7A8u-Ts1XbjhazAkj7I99e8QcYP7DkM=","auth":"tBHItJI5svbpez7KI4CCXg=="}}
-      """
+    subscription = Jason.encode!(@test_subscription)
 
     {:ok, response} = WebPushElixir.send_notification(subscription, "some message")
 
@@ -21,19 +27,39 @@ defmodule WebPushElixirTest do
     assert response.status in 200..202
   end
 
+  test "it should respect the custom ttl argument" do
+    subscription = Jason.encode!(@test_subscription)
+
+    {:ok, response} = WebPushElixir.send_notification(subscription, "some message", ttl: 300)
+
+    assert ["300"] = Map.get(response.request.headers, "ttl")
+  end
+
+  test "it should send the urgency parameter" do
+    subscription = Jason.encode!(@test_subscription)
+
+    {:ok, response} = WebPushElixir.send_notification(subscription, "some message", urgency: :high)
+
+    assert ["high"] = Map.get(response.request.headers, "urgency")
+  end
+
+  test "it should set the topic if it is provided" do
+    subscription = Jason.encode!(@test_subscription)
+
+    {:ok, response} = WebPushElixir.send_notification(subscription, "some message", topic: "some-test-topic")
+
+    assert ["some-test-topic"] = Map.get(response.request.headers, "topic")
+  end
 
   test "it should return expired" do
-    expired_subscription = """
-    {"endpoint":"http://localhost:4040/gone","keys":{"p256dh":"BNcRdreALRFXTkOOUHK1EtK2wtaz5Ry4YfYCA_0QTpQtUbVlUls0VJXg7A8u-Ts1XbjhazAkj7I99e8QcYP7DkM=","auth":"tBHItJI5svbpez7KI4CCXg=="}}
-    """
+    expired_subscription = Jason.encode!(%{@test_subscription | "endpoint" => "http://localhost:4040/gone"})
 
     assert {:error, :expired} = WebPushElixir.send_notification(expired_subscription, "message")
   end
 
   test "it should return http error" do
-    error_subscription = """
-    {"endpoint":"http://localhost:4040/error","keys":{"p256dh":"BNcRdreALRFXTkOOUHK1EtK2wtaz5Ry4YfYCA_0QTpQtUbVlUls0VJXg7A8u-Ts1XbjhazAkj7I99e8QcYP7DkM=","auth":"tBHItJI5svbpez7KI4CCXg=="}}
-    """
+
+    error_subscription = Jason.encode!(%{@test_subscription | "endpoint" => "http://localhost:4040/error"})
 
     assert {:error, {:http_error, status, _body}} = WebPushElixir.send_notification(error_subscription, "message")
     assert status != 200..202
